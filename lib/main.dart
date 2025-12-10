cfe91ce7c274a0d5c138610d80e81887b603a6f2
MouhamedMoukhtarval <moukhtarval@gmail.com>
2025-12-10 12:10:13 +0000

    Change the appearance in the app for testing

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text('3an lkader well l3a6i chi moulana'),
         ),
       ),
     );
