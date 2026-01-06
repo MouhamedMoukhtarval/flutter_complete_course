ed69a13a881a81f4770861d87bac352ad573bdb0
MouhamedMoukhtarval <moukhtarval@gmail.com>
2026-01-06 17:56:23 +0000

    - Completely user authentication token is stored and saved. - We used SharedPrefrence to store token and FlutterSecureStorage to secure token.

---
+++ lib/core/helpers/constants_keys.dart
@@ -0,0 +1,7 @@
+class SharedPreferenceKeys {
+  static const String userToken = "userToken";
+  static const String userEmail = "user_email";
+  static const String userPassword = "user_password";
+}
+
+bool isLoggedIn = false;
--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,14 +1,28 @@
 import 'package:app_serving_doctors/core/di/dependency_injection.dart';
+import 'package:app_serving_doctors/core/helpers/constants_keys.dart';
+import 'package:app_serving_doctors/core/helpers/extensions.dart';
+import 'package:app_serving_doctors/core/helpers/shared_preference_helper.dart';
 import 'package:app_serving_doctors/core/routes/app_router.dart';
 import 'package:app_serving_doctors/doctor_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await ScreenUtil.ensureScreenSize();
+  await checkIfUserLoggedIn();
   await setupGetIt();
-  runApp(DocotrApp(
-    appRouter: AppRouter(),
-  ));
+  runApp(DocotrApp(appRouter: AppRouter()));
+}
+
+Future<void> checkIfUserLoggedIn() async {
+  String? userToken = await SharedPreferenceHelper.getSecureToken(
+    SharedPreferenceKeys.userToken,
+  );
+  if (userToken.isNullOrEmpty()) {
+    isLoggedIn = true;
+  } else {
+    isLoggedIn = false;
+  }
 }
