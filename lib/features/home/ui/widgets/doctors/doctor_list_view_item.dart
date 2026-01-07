19489ba7ee5bc5c5dbe9a70005c3295fd4991dbd
MouhamedMoukhtarval <moukhtarval@gmail.com>
2026-01-07 13:16:40 +0000

    - Some changes on home screen to load picture from internet

--- lib/features/home/ui/widgets/doctors/doctor_list_view_item.dart
+++ lib/features/home/ui/widgets/doctors/doctor_list_view_item.dart
@@ -1,8 +1,11 @@
 import 'package:app_serving_doctors/core/helpers/spacing.dart';
+import 'package:app_serving_doctors/core/theming/colors.dart';
 import 'package:app_serving_doctors/core/theming/styles.dart';
 import 'package:app_serving_doctors/features/home/data/models/specialization_response_model.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shimmer/shimmer.dart';
 
 class DoctorListViewItem extends StatelessWidget {
   final Doctors? doctorsModel;
@@ -11,45 +14,64 @@ class DoctorListViewItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-          margin: EdgeInsets.only(bottom: 16.h),
-          child: Row(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(12.r),
-                child: Image.network(
+      margin: EdgeInsets.only(bottom: 16.h),
+      child: Row(
+        children: [
+          CachedNetworkImage(
+            imageUrl:
+                'https://upload.wikimedia.org/wikipedia/en/4/4b/Abu_Obeida%27s_maskless_face.jpg',
+            progressIndicatorBuilder: (context, url, downloadProgress) {
+              return Shimmer.fromColors(
+                baseColor: ColorsManager.lighterGray,
+                highlightColor: ColorsManager.white,
+                child: Container(
                   width: 110.w,
-                  height: 110.h,
-                  'https://upload.wikimedia.org/wikipedia/en/4/4b/Abu_Obeida%27s_maskless_face.jpg',
-                  fit: BoxFit.cover,
+                  height: 120.h,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(12.0),
+                    color: ColorsManager.white,
+                  ),
                 ),
+              );
+            },
+            imageBuilder: (context, imageProvider) => Container(
+              width: 110.w,
+              height: 120.h,
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(12.0),
+                image: DecorationImage(image: imageProvider, fit: BoxFit.cover),
               ),
-              spacingHorizontal(16),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      doctorsModel?.name ?? 'Doctor',
-                      style: AppStyles.font18DarkBlueBold,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    spacingVertical(8),
-                    Text(
-                      '${doctorsModel?.degree} | ${doctorsModel?.phone}',
-                      style: AppStyles.font12GrayMedium,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    spacingVertical(8),
-                    Text(
-                      doctorsModel?.email ?? 'Email',
-                      style: AppStyles.font12GrayMedium,
-                      overflow: TextOverflow.ellipsis,
-                    )
-                  ]
-                )
-              )
-            ]
-          )
-         );
+            ),
+          ),
+          spacingHorizontal(16),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  doctorsModel?.name ?? 'Doctor',
+                  style: AppStyles.font18DarkBlueBold,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                spacingVertical(8),
+                Text(
+                  '${doctorsModel?.degree} | ${doctorsModel?.phone}',
+                  style: AppStyles.font12GrayMedium,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                spacingVertical(8),
+                Text(
+                  doctorsModel?.email ?? 'Email',
+                  style: AppStyles.font12GrayMedium,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
